a1949db3d35b1a4d6da3326caaa3e0e4519df140
Michael Marcinek <michaelmarcinek@gmail.com>
2015-08-16 22:39:32 -0400

    adds form to submit and action to delete

--- public/js/app.js
+++ public/js/app.js
@@ -10,15 +10,37 @@
     $scope.todoData = {};
 
     // Get all Todos
-    $http.get('/api/v1/todos')
+  $http.get('/api/v1/todos')
+    .success( function(data) {
+      $scope.todoData = data;
+      console.log(data);
+    })
+    .error(function(error) {
+      console.log('Error: ' + error);
+    });
+
+  $scope.createTodo = function(todoId) {
+    $http.post('/api/v1/todos', $scope.formData)
       .success( function(data) {
+        $scope.formData = {};
         $scope.todoData = data;
         console.log(data);
       })
-      .error(function(error) {
+      .error( function(error) {
         console.log('Error: ' + error);
       });
+  }
+
+  $scope.deleteTodo = function(todoId) {
+    $http.delete('api/v1/todos/' + todoId)
+      .success( function (data){
+        $scope.todoData = data;
+        console.log(data);
+      })
+      .error( function(error) {
+        console.log('Error: ' + error);
+      });
+  };
 
   });
-
 }());
